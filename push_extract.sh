e4b4b504b84aeaff5430120ca04dfb4139dfd71f
ben-schulz <benjamin.john.schulz@gmail.com>
2020-07-27 23:32:39 -0500

    feat: add automated push.

---
+++ push_extract.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z $EXTRACT_PATH ]; then
+	echo 'you need to set a value for $EXTRACT_PATH and re-run.' > /dev/stderr
+	exit 1
+fi
+
+datestamp=$(date +%Y-%m-%d)
+csv_glob="*_$datestamp.csv"
+todays_files="$EXTRACT_PATH/$csv_glob"
+
+csv_dest='./data/boone-county-jail'
+mkdir -p $csv_dest
+cp $(ls $todays_files) $csv_dest
+
+git add "$csv_dest/$csv_glob"
+git commit -m 'data: run automated extract.'
+git push origin
